d4410d390679446b6b529d71278cf28921d627d2
Joseph <josephvoss14@gmail.com>
2015-11-12 08:47:19 -0600

    Debugging basic assembler, able to assemble test1.vasm

--- Assembler/main.cpp
+++ Assembler/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <bitset>
+#include <map>
+#include <fstream>
 
 //Usage: assembler <file>
 //assembler <file> <output>
@@ -9,122 +12,78 @@
 //Handle numbers, not just addresses. Already does? Just need to set addy flags for necessary functions
 //Variable names for addresses, not just the raw value;
 //Generate valid command sentences. Possibly change command sentence structure
+//Make it a 2 pass, resolve label names
+//Label map needs offset address, not line counter
 
-std::string commandParser(std::string commandIn)
+std::string commandWordParser(std::string commandIn)
 {
 	std::string output;
-	switch(commandIn)
-	{
-		case "add" : output = "0001"
-					break;
-		case "sub" : output = "0010"
-					break;
-		case "inv" : output = "0011"
-					break;
-		case "mov" : output = "0100"
-					break;
-		case "jfl" : output = "0101"
-					break;
-		case "jfe" : output = "0110"
-					break;
-		case "jfg" : output = "0111"
-					break;
-		default : output = "ERROR"
-					break;
-	}
+	if (commandIn == "add") output = "0001";
+	else if (commandIn == "sub") output = "0010";
+	else if (commandIn == "inv") output = "0011";
+	else if (commandIn == "mov") output = "0100";
+	else if (commandIn == "jfl") output = "0101";
+	else if (commandIn == "jfe") output = "0110";
+	else if (commandIn == "jfg") output = "0111";
+	else output = "LONGERROR";
+	
+	return output;
 }
 
 //Function not needed
 std::string shexTosbin(std::string shex)
 {
 	std::string sbin;
-	switch(shex)
-	{
-		case "0" : sbin = "0000";
-					break;
-		case "1" : sbin = "0001";
-					break;
-		case "2" : sbin = "0010";
-					break;
-		case "3" : sbin = "0011";
-					break;
-		case "4" : sbin = "0100";
-					break;
-		case "5" : sbin = "0101";
-					break;
-		case "6" : sbin = "0110";
-					break;
-		case "7" : sbin = "0111";
-					break;
-		case "8" : sbin = "1000";
-					break;
-		case "9" : sbin = "1001";
-					break;
-		case "A" : sbin = "1010";
-					break;
-		case "B" : sbin = "1011";
-					break;
-		case "C" : sbin = "1100";
-					break;
-		case "D" : sbin = "1101";
-					break;
-		case "E" : sbin = "1110";
-					break;
-		case "F" : sbin = "1111";
-					break;
-		default : sbin = "ERROR";
-					break;
-	}
+
+	if (shex == "0") sbin = "0000";
+	else if (shex == "1") sbin = "0001";
+	else if (shex == "2") sbin = "0010";
+	else if (shex == "3") sbin = "0011";
+	else if (shex == "4") sbin = "0100";
+	else if (shex == "5") sbin = "0101";
+	else if (shex == "6") sbin = "0110";
+	else if (shex == "7") sbin = "0111";
+	else if (shex == "8") sbin = "1000";
+	else if (shex == "9") sbin = "1001";
+	else if (shex == "A") sbin = "1010";
+	else if (shex == "B") sbin = "1011";
+	else if (shex == "C") sbin = "1100";
+	else if (shex == "D") sbin = "1101";
+	else if (shex == "E") sbin = "1110";
+	else if (shex == "F") sbin = "1111";
+	else sbin = "LONGERROR";
+
 	return sbin;
 }
 
-std::string valueToWord(std::string value)
+std::string valueToWord(std::string value, std::map<std::string,std::string> &labelMap)
 {
 	std::string word;
-	switch(value) //need to do this for each value. Pain in the ass if you ask me
-	{
-		case "regA" : word = "100000000";
-						break;
-		case "regB" : word = "100000001";
-						break;
-		case "regC" : word = "100000010";
-						break;
-		case "regD" : word = "100000011";
-						break;
-		case "regE" : word = "100000100";
-						break;
-		case "regF" : word = "100000101";
-						break;
-		case "regG" : word = "100000110";
-						break;
-		case "regH" : word = "100000111";
-						break;
-		case "regI" : word = "100001000";
-						break;
-		case "regJ" : word = "100001001";
-						break;
-		case "regK" : word = "100001010";
-						break;
-		case "regL" : word = "100001011";
-						break;
-		case "regM" : word = "100001100";
-						break;
-		case "regN" : word = "100001101";
-						break;
-		case "regO" : word = "100001110";
-						break;
-		case "regP" : word = "100001111";
-						break;
-		default : word = "ERROR";
-						break;
-	}
 
-	long wordNum
-	if (word == "ERROR")
+	if (value == "regA") 	  word = "100000000";
+	else if (value == "regB") word = "100000001";
+	else if (value == "regC") word = "100000010";
+	else if (value == "regD") word = "100000011";
+	else if (value == "regE") word = "100000100";
+	else if (value == "regF") word = "100000101";
+	else if (value == "regG") word = "100000110";
+	else if (value == "regH") word = "100000111";
+	else if (value == "regI") word = "100001000";
+	else if (value == "regJ") word = "100001001";
+	else if (value == "regK") word = "100001010";
+	else if (value == "regL") word = "100001011";
+	else if (value == "regM") word = "100001100";
+	else if (value == "regN") word = "100001101";
+	else if (value == "regO") word = "100001110";
+	else if (value == "regP") word = "100001111";
+	else word = "LONGERROR";
+
+	long wordNum;
+	if (word == "LONGERROR")
 	{
 		try
 		{
-			wordNum = stol(word);
+			wordNum = std::stoi(word, NULL, 0);
 			if (wordNum < 255 && wordNum > 0) //if 8 bit. If value should actually be 16 bit can buffer it in the mov function
 				word = std::bitset<9>(wordNum).to_string(); //includes register bit set to 0
 			else
@@ -132,14 +91,14 @@ std::string valueToWord(std::string value)
 		}
 		catch(...)
 		{
-			word = "ERROR";
+			word = "LONGERROR";
 		}	
 	}
-	if (word == "ERROR")
+	if (word == "LONGERROR")
 	{
 		word = labelMap[word];
 		if (word == "")
-			word = "ERROR";
+			word = "LONGERROR";
 	}
 
 	return word;
@@ -148,25 +107,25 @@ std::string valueToWord(std::string value)
 int main(int argc, char* argv[])
 {
 	bool validInput = true;
-	if (argc > 3 || argc == 0)
+	if (argc > 3 || argc == 1)
 	{
-		printHelp();
+	//	printHelp();
 		validInput = false;
 	}
 
-	if (argc == 1) argv[1] = "./a.out";
+	if (argc == 2) argv[2] = (char*) "./a.out";
 
-	ifstream inputFile;
-	ofstream outputFile;
+	std::ifstream inputFile;
+	std::ofstream outputFile;
 
 	try
 	{
-		inputFile.open(argv[0]);
-		outputFile.open(argv[1]);
+		inputFile.open(argv[1]);
+		outputFile.open(argv[2]);
 	}
 	catch(...)
 	{
-		printf("Input files are not valid");
+		std::cout<<"Input files are not valid"<<std::endl;
 		validInput = false;
 	}
 
@@ -177,7 +136,7 @@ int main(int argc, char* argv[])
 	std::string assemblyLine, commandWord, value1Word, value2Word, value3Word;
 	std::string command, value1, value2, value3;
 	int valueCounter = 0;
-	std::map<std::string, int> labelMap;
+	std::map<std::string, std::string> labelMap;
 	
 	//Correct syntax
 	// lbl value1
@@ -186,7 +145,7 @@ int main(int argc, char* argv[])
 	// inv value1 value2
 	// jf  value1 value2
 	// int var = 0;?
-	while(validInput && line << inputFile)
+	while(std::getline(inputFile, line) && validInput && line != ";")
 	{
 		try
 		{
@@ -194,40 +153,46 @@ int main(int argc, char* argv[])
 			//if(line.find(';') == NULL) throw;
 			//line.erase(0, line.find(';'));
 			command = line.substr(0, line.find(' '));
-			line.erase(0, line.find(' '));
-			valueCounter = 0
+			line.erase(0, line.find(' ')+1);
+			valueCounter = 0;
 			value1 = line.substr(0, line.find(' ')); //what if ends with;? can use ' ;' and endl synatx?
+			value1Word = valueToWord(value1, labelMap);
 			valueCounter++;
-			line.erase(0, line.find(' '));
+			line.erase(0, line.find(' ')+1);
 			try
 			{
 				value2 = line.substr(0, line.find(' '));//endline instead?
-				value2Word = valueToWord(value2);
+				value2Word = valueToWord(value2, labelMap);
 				valueCounter++;
-				line.erase(0, line.find(' '));
-				value3 = line.substr(0, line,find(' '));//endline instead?
-				value3Word = valueToWord(value3);
-				value3Counter++;
+				line.erase(0, line.find(' ')+1);
+				value3 = line.substr(0, line.find(' '));//endline instead?
+				value3Word = valueToWord(value3, labelMap);
+				valueCounter++;
 				//erase is unnecessary, line not used
 			}
 			catch(...)
 			{
 				if(command == "lbl")
 				{
-					labelMap[value1] = lineCounter; //should this be the address to jump to?
+					labelMap[value1] = std::to_string(lineCounter); //should this be the address to jump to?
 				}
+				if (line == ";") std::cout<<"End of line " <<lineCounter<<" reached\n";
 			}
 			
 			//value checking && generate sentence
 
-			assemblyLine = "ERROR";
+			assemblyLine = "LONGERROR";
 			commandWord = commandWordParser(command);
 			if (command == "sub" || command == "add") //only ternary commands
 			{
 				if (valueCounter != 3)
 					std::cout<<"Wrong number of arguements for command on line "<<lineCounter<<std::endl;
-				assemblyLine = commandWord + value1Word + value2Word + value3Word[6:9]; //don't include first 5 bits for word 3
+				assemblyLine = commandWord + value1Word + value2Word + value3Word.substr(6,4) + "\n"; //don't include first 5 bits for word 3
 			}
+
+			if (command == "lbl")
+				if (valueCounter != 1)
+					std::cout<<"Wrong number of arguements for command on line "<<lineCounter<<std::endl;
 
 			else
 			{
@@ -236,22 +201,24 @@ int main(int argc, char* argv[])
 			}
 
 			if (command == "inv")
-				assemblyLine = commandWord + value1Word + value2Word[6:9] + "000000000";
+				assemblyLine = commandWord + value1Word + value2Word.substr(6,4) + "000000000";
 
 			if (command == "mov");
 
 			if (command == "jfl" || command == "jfe" || command == "jfg")
 			{
-				if (value2Word.length < 16)
+				if (value2Word.length() < 16)
 					assemblyLine = commandWord + value1Word + "00000000" + value2Word + "00";
 				else
 					assemblyLine = commandWord + value1Word + value2Word + "00";
 			}
+			outputFile << assemblyLine;
+			std::cout<<"Line "<<lineCounter<<" successfully parsed"<<std::endl;
 
 		}
 		catch(...)
 		{
-			printf("Malformed statement at line %d", lineCounter):
+			std::cout<<"Malformed statement at line "<<lineCounter<<std::endl;;
 		}
 		lineCounter += 1;
 	}	
